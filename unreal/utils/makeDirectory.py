ca07f8d97f37bd742e644301f852c170120c0ae4
merwynl <maawin.rimu@gmail.com>
2019-06-22 22:09:59 +1000

    create a list of sub dirs & prefixes rename function add test project

--- unreal/utils/makeDirectory.py
+++ unreal/utils/makeDirectory.py
@@ -1,13 +1,36 @@
 import unreal
+import json
 
-directory_path = "/Game/Content"
+project_name = "001_Test" 
+prefix = ["SE", "SV"]
+sub_directories = ["materials", "textures", "meshes", "blueprints"]
+
 directory = unreal.EditorAssetLibrary()
+directory_path = "/Game/" + prefix[0] + project_name
 
-def makeDirectory():
-    if directory.does_directory_exist == True:
-        print str(directory_path)+' already exists'
+level = unreal.EditorLevelLibrary()
+level_name = directory_path + prefix[1] + project_name
+
+def make_directory():
+    does_exist = True
+    if directory.does_directory_exist == does_exist:
+        print(str(directory_path) + ' already exists')
     else:
         directory.make_directory(directory_path)
-        print str(directory_path)+' created'
+        level.new_level(level_name)
+        for i in sub_directories:
+            directory.make_directory(directory_path)
+        does_exist = True
+        print(str(directory_path) + ' created')
 
-makeDirectory()
+# def make_level():
+#     directory_exists = True
+#     if directory.does_directory_exist == directory_exists:
+#         level.new_level(level_name)
+#     else:
+#         level.new_level(directory_path) 
+#         print(str(directory_path) + ' created')
+
+
+
+make_directory()
