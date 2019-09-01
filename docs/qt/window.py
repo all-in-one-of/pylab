96131de51f77f9491918100204a71be69967fb2c
merwynl <maawin.rimu@gmail.com>
2019-09-01 10:58:17 +1000

    remove test stuff

--- docs/qt/window.py
+++
@@ -1,9 +0,0 @@
-
-
-"""
-[Creating a window using Pyside2]
-"""
-import PySide2
-print PySide2
-win = PySide2.QtWidgets.QDialog()
-win.show()
--- tests/app.py
+++
@@ -1,3 +0,0 @@
-x = input("X:")
-print (type(x))
-# y = x + 1
